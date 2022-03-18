7325c166666930768f461c1ef7ef6f88d35d287f
oceanheart99 <oceanheart99@gmail.com>
2022-03-18 22:09:25 +0800

    Update pub_tx.js

--- pub_tx.js
+++ pub_tx.js
@@ -2,8 +2,8 @@ const { ethers } = require("ethers");
 
 async function main () {
 //source is cold wallet
-src_wallet_address = "0x0765919FCB7beAEc6922c2086180751D0f0D55A3"
-dst_wallet_address = "0x2A2A64ff0a51257fE4115a3cC1bf94687F13223d"
+src_wallet_address = "xxx" //need to change to your src cold wallet address
+dst_wallet_address = "xxx" //need to change to your dst cold wallet address
 //check initial balance
 const myprovider = ethers.getDefaultProvider("mainnet")
 console.log("Before transaction:")
@@ -17,7 +17,7 @@ console.log("dst balance:", dst_bal_eth)
 
 // publish transaction online
 //console.log(await myprovider.getGasPrice())
-//const signed_tx = "0xf86b808506fc23ac0082c350942a2a64ff0a51257fe4115a3cc1bf94687f13223d871550f7dca70000801ca01d69f39bdbeb62ad793688166a666cb18f005eb62e44b00938b007b8c289aa44a04a1fc0d7236e3ebf1dd241f782a6aca433e3e48643f7602dd7bec81caa2d6a43"
+//const signed_tx = "xxx" ////need to change to your signed transaction generated offline
 console.log("Wait for transaction confirmation...")
 resp = await myprovider.sendTransaction(signed_tx)
 await myprovider.waitForTransaction(resp.hash)
